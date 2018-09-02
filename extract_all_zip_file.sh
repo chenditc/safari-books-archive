5c6cb9075d5b0ee98668c0463c0bb66a53d7ffa1
Di Chen <chen.di@hyfield.com>
2018-09-02 21:04:15 +0800

    Add data mining

---
+++ extract_all_zip_file.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+set -x
+
+for zip_file in $(ls zip_files)
+do
+    ./extract_zip_file.sh zip_files/$zip_file
+done
+
--- extract_zip_file.sh
+++ extract_zip_file.sh
@@ -7,9 +7,9 @@ if [ -f "$zip_file" ]
 then
     mkdir -p /tmp/safari-book/
     unzip -o -d /tmp/safari-book/ $zip_file 
-    for mhtml_file in $(ls /tmp/safari-book/*/*.mhtml)
+    ls /tmp/safari-book/*/*.mhtml /tmp/safari-book/*.mhtml | while read mhtml_file
     do
-        ./mhtml_to_html_converter.py $mhtml_file ./site 
+        ./mhtml_to_html_converter.py "$mhtml_file" ./site 
     done
     rm -rf /tmp/safari-book/
 else
