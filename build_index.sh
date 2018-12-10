0e8d959e9fcb13b436135adc161b5ef42b7bfe02
Di Chen <chen.di@hyfield.com>
2018-12-10 15:15:53 +0800

    Split book name and chap name

--- build_index.sh
+++ build_index.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 rm index.html
 find ./site -name '*.html' | sort | while read path
 do
-    echo $path | sed 's/\.\///g' | python3 -c "relative_path=input(); name=relative_path.replace('site/library/view/', ''); print('<h3><a href={0}>{1}</a></h3>'.format(relative_path, name))" >> index.html
+    echo $path | sed 's/\.\///g' | python3 -c "relative_path=input(); name=relative_path.replace('site/library/view/', ''); book_name = name.split('/')[0]; chap_name=name.split('/')[-1];  print('<h3>{2}: <a href={0}>{1}</a></h3>'.format(relative_path, chap_name, book_name))" >> index.html
 done
