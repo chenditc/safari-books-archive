49926502b493249f1b651a165675ed115f70ad7d
Di Chen <chen.di@hyfield.com>
2018-12-11 10:58:14 +0800

    Change index pattern

--- build_index.sh
+++ build_index.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-rm index.html
+rm *.html
+rm ./index/*.html
+
+# Create per book index
 find ./site -name '*.html' | sort | while read path
 do
-    echo $path | sed 's/\.\///g' | python3 -c "relative_path=input(); name=relative_path.replace('site/library/view/', ''); book_name = name.split('/')[0]; chap_name=name.split('/')[-1];  print('<h3>{2}: <a href={0}>{1}</a></h3>'.format(relative_path, chap_name, book_name))" >> index.html
+    bookname=$(echo $path | sed 's/\.\///g' | cut -d "/" -f 4) 
+    echo $path | sed 's/\.\///g' | python3 -c "relative_path=input(); name=relative_path.replace('site/library/view/', ''); book_name = name.split('/')[0]; chap_name=name.split('/')[-1];  print('<h3>{2}: <a href={0}>{1}</a></h3>'.format(relative_path, chap_name, book_name))" >> ./index/${bookname}.html
 done
+
+# Create total index
+find ./index -name '*.html' | sort | while read path
+do
+    echo $path | sed 's/\.\/index\///g' | python3 -c "relative_path=input(); print('<h3><a href=index/{0}>{0}</a></h3>'.format(relative_path))" >> ./index.html
+done
+
